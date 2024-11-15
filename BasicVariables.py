85cbce00be1e4ec739f413cc2d041c0ebf1db3bc
Dipesh Mahato <D.M.tharu@wlv.ac.uk>
2024-11-15 07:21:08 -0800

    msg

---
+++ BasicVariables.py
@@ -0,0 +1,10 @@
+# Basic Variables:
+# Create a variable named city and assign the name of your favorite city as a string.
+# Create another variable named population and assign a number (integer) to it.
+# Print both variables in a single sentence.
+
+city="Kathmandu"
+population=19992121
+
+print(city)
+print(population)
---
+++ indentation.py
@@ -0,0 +1,5 @@
+# Python Indentation
+if 7 >5 :
+    print(" 7 is greater")
+else :
+    print(" 7 is not greater ")
